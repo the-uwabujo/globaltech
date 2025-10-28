6a9a7b9858a789e0bc2d374dc374052820c0c975
elbelel <umarelbelel@gmail.com>
2025-10-28 14:33:32 +0100

    model creation for user

--- globalapp/models.py
+++ globalapp/models.py
@@ -1,9 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, AbstractUser, UserManager, PermissionsMixin
+import uuid
+
 
 # Create your models here.
 
-class Account(models.Model):
-      email = models.EmailField(unique=True, max_length=50)
-      first_name = models.CharField(max_length=255, null=True)
-      last_name = models.CharField(max_length=255, null=True)
-      Date = models.DateTimeField()
+class AccountUserManager(BaseUserManager):
+    """
+        This is a manager for Account class
+    """
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class Account(AbstractBaseUser, PermissionsMixin):
+
+    id = models.UUIDField(primary_key=True,
+                          default=uuid.uuid4,
+                          editable=False)
+    email = models.EmailField(verbose_name="email", max_length=60, unique=True)
+    first_name = models.CharField(max_length=255, default=None, blank=True, null=True)
+    last_name = models.CharField(max_length=255, default=None, blank=True, null=True)
+    phone_number = models.CharField(max_length=30, default=None, blank=True, null=True)
+    last_login = models.DateTimeField(verbose_name="last login", auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    # REQUIRED_FIELDS = ['username']
+    objects = AccountUserManager()
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.email
+
+    # def has_perm(self, perm, obj=None):
+    #     return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
+
+
+
--- soclinqsec/settings.py
+++ soclinqsec/settings.py
@@ -39,6 +39,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+AUTH_USER_MODEL = 'globalapp.Account'
+
 ROOT_URLCONF = 'soclinqsec.urls'
 
 TEMPLATES = [
@@ -65,16 +67,20 @@ WSGI_APPLICATION = 'soclinqsec.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'soclinqglobaltech',
-        'USER': 'root',
-        'PASSWORD': 'System@123',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-          'sql_mode':'STRICT_TRANS_TABLES'
-        }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'soclinqglobaltech',
+    #     'USER': 'root',
+    #     'PASSWORD': 'System@123',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
+    #     'OPTIONS': {
+    #       'sql_mode':'STRICT_TRANS_TABLES'
+    #     }
+    # }
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
